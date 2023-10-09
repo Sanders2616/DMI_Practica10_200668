ba88b20ba88f3b79f763e98b5824d18b0572dc6e
Sanders2616 <sandra.aguilarsantos@utxicotepec.edu.mx>
2023-10-09 11:19:53 -0600

    first commit

---
+++ practica10_200668/android/app/src/main/kotlin/com/example/practica10_200668/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practica10_200668
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ practica10_200668/lib/media_list_item.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:practica8_200668/model/Media.dart';
+
+class MediaListItem extends StatelessWidget {
+  final Media media;
+  MediaListItem(this.media);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              FadeInImage.assetNetwork(
+                placeholder: "assets/imagen.png",
+                image: media.getBackDropUrl(),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 200.0,
+                fadeInDuration: Duration(milliseconds: 50),
+              ),
+              new Positioned(
+                left: 0.0,
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.pink.withOpacity(0.5),
+                  ),
+                  constraints: BoxConstraints.expand(height: 55.0),
+                ),
+              ),
+              Positioned(
+                left: 10.0,
+                bottom: 10.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 300.0,
+                      child: Text(
+                        media.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 4.0),
+                      child: Text(
+                        media.getGenres(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                  right: 7.5,
+                  bottom: 10.0,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(media.voteAverage.toString()),
+                          Container(width: 4.0),
+                          Icon(Icons.star, color: Colors.white, size: 16.0)
+                        ],
+                      ),
+                      Container(
+                        height: 4.0,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(media.getReleaseYear().toString()),
+                          Container(
+                            width: 4.0,
+                          ),
+                          Icon(Icons.date_range,
+                              color: Colors.white, size: 16.0)
+                        ],
+                      )
+                    ],
+                  ))
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
